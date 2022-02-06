97fc19a68f78a1295182f2b4f8b5c00d0702652c
Arthur Kalule <kalulearthur@gmail.com>
2022-02-06 16:19:04 +0000

    create a browser client

---
+++ src/main/resources/static/app.js
@@ -0,0 +1,42 @@
+var stompClient = null;
+
+const setConnected = (connected) => {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if(connected){
+        $("#conversation").show();
+    } else {
+        $("#conversation").hide();
+    }
+    $("#greetings").html("");
+}
+
+const connect = () => {
+    var socket = new SockJS('/gs-guide-websocket');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, (frame) => {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/greetings', greeting =>    showGreeting(JSON.parse(greeting.body).content));
+    });
+}
+
+const  disconnect = () => {
+    if(stompClient != null){
+        stompClient.disconnect();
+    }
+
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+const sendName = () => stompClient.send('/app/hello', {}, JSON.stringify({'name': $('name').val()}));
+
+const showGreeting = (message) => $('#greeetings').append("<tr><td>" + message + "</td></tr>");
+
+$( () => {
+    $("form").on('submit', (e) => e.preventDefault());
+    $("#connect").click( () => connect());
+    $("#disconnect").click( () => disconnect());
+    $("#send").click( () => sendName());
+});
