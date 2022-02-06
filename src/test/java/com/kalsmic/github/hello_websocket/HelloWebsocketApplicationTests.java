9ed94109b940dca3f2d4345e2bc0c35e2d030f18
Arthur Kalule <arthurk@lgm.43qeqktlb2fu3huyl0p2dcdr5e.bx.internal.cloudapp.net>
2022-02-06 04:50:06 +0000

    initialize spring project

---
+++ src/test/java/com/kalsmic/github/hello_websocket/HelloWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalsmic.github.hello_websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
